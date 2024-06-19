965e2f6ce321edc1231ce5dca0aa379ec026e28b
kanishksaini12 <68396302+kanishksaini12@users.noreply.github.com>
2024-06-19 10:44:03 +0530

    navbar added

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,40 @@
+import { useEffect, useState } from "react";
 import { TracingBeam } from "../../ui/tracing-beam";
 import AboutMe from "../aboutMe/AboutMe";
+import Navbar from "../navbar/Navbar";
 import Welcome from "../welcome/Welcome";
 import "./Layout.css";
 
 function Layout() {
+  const [active, setActive] = useState<string | null>("Home");
+
+  useEffect(() => {
+    if (active) {
+      const a = active.toLowerCase();
+      const id = document.getElementById(a as string);
+      id?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      });
+    }
+    setActive(null);
+  }, [active]);
+
   return (
-    <TracingBeam className="px-6 ml-[5em]">
-      <div className="section overflow-x-hidden">
-        <Welcome />
+    <div>
+      <div>
+        <Navbar active={active} setActive={setActive} />
       </div>
-      <div className="section">
-        <AboutMe />
-      </div>
-    </TracingBeam>
+      <TracingBeam className="px-6 ml-[5em]">
+        <div id="home" className="section overflow-x-hidden">
+          <Welcome />
+        </div>
+        <div id="about" className="section">
+          <AboutMe />
+        </div>
+      </TracingBeam>
+    </div>
   );
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+//import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+
+import { useState } from "react";
+import { Menu, MenuItem } from "../../ui/navbar-menu";
+import { cn } from "../../utils/cn";
+import "./Navbar.css";
+
+function Navbar({ active, setActive }: any) {
+  function setActiveMenu(event: any) {
+    setActive(event);
+  }
+
+  return (
+    <div className={cn("fixed top-8 inset-x-0 max-w-[25em] mx-auto z-50")}>
+      <Menu setActive={setActiveMenu}>
+        <MenuItem setActive={setActiveMenu} active={active} item="Home" />
+        <MenuItem setActive={setActiveMenu} active={active} item="About" />
+        <MenuItem setActive={setActiveMenu} active={active} item="Contact" />
+        <MenuItem setActive={setActiveMenu} active={active} item="Resume" />
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
